3caa1ad7155d3d9d80f6e7c94bf5dcac493a3486
hecobrith <hecobrith@gmail.com>
2019-11-09 23:27:26 -0600

    adding the service of sending messages to e-mail when the store fulfillment

--- handler.js
+++ handler.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const orderManager = require('./orderManager');
+const kinesisHelper = require('./kinesisHelper');
+const cakeProducerManager = require('./cakeProducerManager');
+const deliveryManager = require('./deliveryManager');
 
 function createResponse(statusCode, message) {
   const response = {
@@ -33,4 +36,38 @@ module.exports.orderFulfillment = async (event) => {
   }).catch(error => {
     return createResponse(400, error);
   })
+}
+
+// here the lambda is getting the  event from the kinesis data stream
+module.exports.notifyExternalParties = async (event) => {
+  const records = kinesisHelper.getRecords(event);
+
+  const cakeProducerPromise = getCakeProducerPromise(records);
+  const deliveryPromise = getDeliveryPromise(records);
+
+  return Promise.all([cakeProducerPromise, deliveryPromise]).then(() => {
+    return 'everything went well'
+  }).catch(error => {
+    return error;
+  })
+}
+
+function getCakeProducerPromise(records) {
+  const ordersPlaced = records.filter(r => r.eventType === 'order_placed');
+
+  if (ordersPlaced.length > 0) {
+    return cakeProducerManager.handlePlacedOrders(ordersPlaced);
+  } else {
+    return null;
+  }
+}
+
+function getDeliveryPromise(records) {
+  const orderFulfilled = records.filter(r => r.eventType === 'order_fulfilled');
+
+  if (orderFulfilled.length > 0) {
+    return deliveryManager.deliveryOrder(orderFulfilled);
+  } else {
+    return null;
+  }
 }
